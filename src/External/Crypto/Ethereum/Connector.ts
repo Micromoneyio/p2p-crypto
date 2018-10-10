d2ebb62d4ccfe9b35f1df58dc157a28786711b89
egorikas <egorgrishechko@gmail.com>
2018-10-10 23:31:27 +0300

    https://trello.com/c/EaBuPfav fix

--- src/External/Crypto/Ethereum/Connector.ts
+++ src/External/Crypto/Ethereum/Connector.ts
@@ -1,4 +1,4 @@
 const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider(process.env.ETH_NODE || "https://mew.giveth.io"));
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.ETH_NODE || "https://mainnet.infura.io/mew"));
 
 export default web3;
